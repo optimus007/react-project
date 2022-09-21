b4ed3bed5f863143a65ffaeb5f02835aeb40a389
optimus007 <optimus007@users.noreply.github.com>
2022-09-21 22:50:48 +0530

    bruh

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import "./App.css";
 import { Canvas, useFrame, ThreeElements, useLoader } from "@react-three/fiber";
 import { useRef, useState, Suspense, useEffect } from "react";
-import { Environment, OrbitControls, PerformanceMonitor, Stats, useGLTF, useAnimations, ContactShadows } from "@react-three/drei";
+import { Environment, OrbitControls, PerformanceMonitor, Stats, useGLTF, useAnimations, ContactShadows, Instances, Instance } from "@react-three/drei";
 import { useControls } from "leva";
 import { version } from "../package.json";
-import round from "lodash/round";
+import { Color, MathUtils } from "three";
 
 function GuiStuff() {
   const ver = useControls({
     version: String(version),
   });
 }
+const params = {
+  range: 0,
+};
 
 function Box(props: ThreeElements["mesh"]) {
   const mesh = useRef<THREE.Mesh>(null!);
@@ -49,6 +52,45 @@ const Model = () => {
   );
 };
 
+const color = new Color();
+const randomVector = (r: number) => [r / 2 - Math.random() * r, r / 2 - Math.random() * r + r / 2, r / 2 - Math.random() * r];
+const randomEuler = () => [Math.random() * Math.PI, Math.random() * Math.PI, Math.random() * Math.PI];
+const randomData = Array.from({ length: 1000 }, (r = 5) => ({ random: Math.random(), position: randomVector(r), rotation: randomEuler() }));
+
+const Shoes = ({ range }) => {
+  const gltf = useGLTF("./shoe.glb");
+  const { nodes, materials } = gltf;
+
+  return (
+    <Instances range={range} material={materials.phong1SG} geometry={nodes.Shoe.geometry}>
+      {randomData.map((props, i) => (
+        <Shoe key={i} {...props} />
+      ))}
+    </Instances>
+  );
+};
+
+function Shoe({ random, ...props }) {
+  const ref = useRef<Instance>();
+  const [hovered, setHover] = useState(false);
+
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime() + random * 10000;
+    ref.current.rotation.set(Math.cos(t / 4) / 2, Math.sin(t / 4) / 2, Math.cos(t / 1.5) / 2);
+    ref.current.position.y = Math.sin(t / 1.5) / 2;
+    ref.current.scale.x = ref.current.scale.y = ref.current.scale.z = MathUtils.lerp(ref.current.scale.z, hovered ? 1 : 0.5, 0.1);
+    ref.current.color.lerp(color.set(hovered ? "red" : "white"), hovered ? 1 : 0.1);
+  });
+  return (
+    <group {...props}>
+      <Instance
+        ref={ref}
+        // onPointerOver={(e) => (e.stopPropagation(), setHover(true))} onPointerOut={(e) => setHover(false)}
+      />
+    </group>
+  );
+}
+
 function ThreeScene() {
   const [dpr, setDpr] = useState(1);
 
@@ -63,21 +105,23 @@ function ThreeScene() {
     scale: 20,
   });
 
+  // const canvasProps = useControls("Canvas", {
+  //   dpr: dpr,
+  // });
+  const { range } = useControls({ range: { value: 10, min: 0, max: 500, step: 10 } });
   return (
     <Canvas dpr={dpr} style={{ background: "grey" }} camera={{ position: [0, 2, 5] }}>
       <Stats />
       <PerformanceMonitor
-        // onIncline={() => {
-        //   setDpr(2)
-        //   console.log("Incline")
-        // }}
-        // onDecline={() => {
-        //   setDpr(1)
-        //   console.log("Decline")
-        // }}
-        onChange={({ factor }) => {
-          setDpr(round(0.5 + 1.5 * factor, 1));
-          console.log({ factor, dpr });
+        iterations={3}
+        onIncline={() => {
+          setDpr(Math.min(dpr + 0.1, window.devicePixelRatio));
+
+          console.log("Incline", dpr);
+        }}
+        onDecline={() => {
+          setDpr(Math.max(dpr - 0.1, 0.5));
+          console.log("Decline", dpr);
         }}
       />
       <OrbitControls target={[0, 1, 0]} />
@@ -105,6 +149,8 @@ function ThreeScene() {
           far={10}
           resolution={256}
         />
+
+        <Shoes range={range} />
       </Suspense>
     </Canvas>
   );
