409fbfaf0eae2fb16401a551f359bd794380bc9b
optimus007 <optimus007@users.noreply.github.com>
2022-09-28 00:07:38 +0530

    water test

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import "./App.css";
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Environment, PerformanceMonitor } from "@react-three/drei";
-import { useControls } from "leva";
-import { Robot } from "./assets/Robot";
-import { TweakBox } from "./assets/TweakBox";
-import { Shoes } from "./assets/Shoes";
-import { BoxesSpread } from "./assets/BoxesSpread";
-import { Lanterns } from "./assets/Lantern";
-import Grass from "./assets/Grass";
+import "./App.css"
+import { Suspense } from "react"
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls, Environment, PerformanceMonitor, MeshReflectorMaterial } from "@react-three/drei"
+import { useControls } from "leva"
+import { Robot } from "./assets/Robot"
+import { TweakBox } from "./assets/TweakBox"
+import { Shoes } from "./assets/Shoes"
+import { BoxesSpread } from "./assets/BoxesSpread"
+import { Lanterns } from "./assets/Lantern"
+import Grass from "./assets/Grass"
+import { Water } from "./assets/Water"
 
 export default function App() {
   const [{ dpr }, setDpr] = useControls("Px Ratio", () => ({
@@ -18,30 +19,32 @@ export default function App() {
       max: window.devicePixelRatio,
       step: 0.2,
     },
-  }));
+  }))
 
   const groundProj = useControls("Ground", {
-    height: 5,
-    radius: 40,
-    scale: 20,
-  });
+    height: 15,
+    radius: 50,
+    scale: 800,
+  })
 
   return (
     <Canvas dpr={dpr} camera={{ position: [0, 2, 5], fov: 80 }}>
       <PerformanceMonitor
         iterations={3}
         onIncline={() => {
-          setDpr(Math.min(dpr + 0.1, window.devicePixelRatio));
+          setDpr(Math.min(dpr + 0.1, window.devicePixelRatio))
 
-          console.log("Incline", dpr);
+          console.log("Incline", dpr)
         }}
         onDecline={() => {
-          setDpr(Math.max(dpr - 0.1, 0.5));
-          console.log("Decline", dpr);
+          setDpr(Math.max(dpr - 0.1, 0.5))
+          console.log("Decline", dpr)
         }}
       />
       {/* <BoxesSpread /> */}
       {/* <TweakBox /> */}
+
+      <Water />
       <Suspense fallback={null}>
         {/* <Shoes /> */}
         <Environment
@@ -54,10 +57,11 @@ export default function App() {
         />
         {/* <Robot /> */}
         <Lanterns />
-        {/* <Grass /> */}
       </Suspense>
+
       <gridHelper />
+
       <OrbitControls target={[0, 1, 0]} />
     </Canvas>
-  );
+  )
 }
---
+++ src/assets/Water.jsx
@@ -0,0 +1,121 @@
+import { MeshReflectorMaterial, useTexture } from "@react-three/drei"
+import { useFrame } from "@react-three/fiber"
+import { folder, useControls } from "leva"
+import { useEffect } from "react"
+import { RepeatWrapping } from "three"
+
+export function Water() {
+  const texture = useTexture("./water_norm.jpg", (texture) => {
+    texture.wrapS = texture.wrapT = RepeatWrapping
+  })
+
+  const [{ color, res, blurX, blurY, rou, met, mixStrength, mixBlur, minDepthThreshold, depthScale, repeat, distortion }] = useControls(
+    "Water",
+    () => ({
+      material: folder({
+        color: "#111414",
+        rou: {
+          value: 1,
+          min: 0,
+          max: 1,
+        },
+        met: {
+          value: 0.5,
+          min: 0,
+          max: 1,
+        },
+      }),
+      res: {
+        value: 1024,
+        min: 256,
+        max: 2048,
+        step: 128,
+      },
+
+      blurX: {
+        value: 400,
+        min: 0,
+        max: 2048,
+        step: 1,
+      },
+      blurY: {
+        value: 100,
+        min: 0,
+        max: 2048,
+        step: 1,
+      },
+
+      mixStrength: {
+        value: 15,
+        min: 0,
+        max: 30,
+        step: 0.1,
+      },
+
+      mixBlur: {
+        value: 1,
+        min: 0,
+        max: 3,
+        step: 0.1,
+      },
+
+      minDepthThreshold: {
+        value: 0.4,
+        min: 0,
+        max: 1,
+        step: 0.1,
+      },
+
+      depthScale: {
+        value: 1,
+        min: 0,
+        max: 30,
+        step: 0.1,
+      },
+
+      repeat: {
+        value: 30,
+        min: 1,
+        max: 30,
+        step: 1,
+      },
+
+      distortion: {
+        value: 0.1,
+        min: 0,
+        max: 3,
+        step: 0.01,
+      },
+    })
+  )
+
+  useEffect(() => {
+    texture.repeat.set(repeat, repeat)
+    texture.needsUpdate = true
+  }, [repeat])
+
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime()
+    texture.offset.set(t * 0.01, Math.sin(t * 0.01))
+  })
+
+  return (
+    <mesh position={[0, -0.001, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <circleGeometry args={[25, 64]} />
+      <MeshReflectorMaterial
+        blur={[blurX, blurY]}
+        resolution={res}
+        mixBlur={mixBlur}
+        mixStrength={mixStrength}
+        depthScale={depthScale}
+        minDepthThreshold={minDepthThreshold}
+        color={color}
+        metalness={met}
+        roughness={rou}
+        normalMap={texture}
+        distortion={distortion}
+        distortionMap={texture}
+      />
+    </mesh>
+  )
+}
